579d90b1f8f0eee840a7800db286cd60b75d3863
Wojciech Kusa <wojciech.kusa@gmail.com>
2021-08-04 13:39:57 +0200

    feat(data): add xml file parser for clinical trials data

---
+++ trec_cds/data/clinical_trial.py
@@ -0,0 +1,21 @@
+class ClinicalTrial:
+    def __init__(
+        self,
+        org_study_id,
+        summary,
+        description,
+        criteria,
+        gender,
+        minimum_age,
+        maximum_age,
+        healthy_volunteers,
+    ):
+        self.org_study_id = org_study_id
+        self.summary = summary
+        self.description = description
+
+        self.criteria = criteria
+        self.gender = gender
+        self.minimum_age = minimum_age
+        self.maximum_age = maximum_age
+        self.healthy_volunteers = healthy_volunteers
--- trec_cds/main.py
+++ trec_cds/main.py
@@ -1,22 +1,66 @@
+import os
 import xml.etree.ElementTree as ET
+from glob import glob
+from typing import List, Union
 
-from src.data.topic import Topic
+from trec_cds.data.clinical_trial import ClinicalTrial
+from trec_cds.data.topic import Topic
 
 
-def parse_topics_from_xml(topic_file):
+def parse_topics_from_xml(topic_file: str) -> List[Topic]:
+    """Parses topics from single XML file and creates a Topic class instance for each parsed item.
+
+    :param topic_file: str
+    :return: List of topics
+    """
     tree = ET.parse(topic_file)
     root = tree.getroot()
 
     topics = []
     for elem in root:
-        topics.append(Topic(elem.attrib["number"], elem.text))
+        topics.append(Topic(number=elem.attrib["number"], text=elem.text))
 
     return topics
 
 
+def parse_clinical_trials_from_folder(
+    folder_name: str, first_n: Union[None, int] = None
+) -> List[ClinicalTrial]:
+    files = [y for x in os.walk(folder_name) for y in glob(os.path.join(x[0], "*.xml"))]
+
+    if first_n:
+        files = files[:first_n]
+
+    clinical_trials = []
+    for file in files:
+        tree = ET.parse(file)
+        root = tree.getroot()
+
+        org_study_id = root.find("id_info").find("org_study_id").text
+
+        eligibility = root.find("eligibility")
+        clinical_trials.append(
+            ClinicalTrial(
+                org_study_id=org_study_id,
+                summary=root.find("brief_summary")[0].text,
+                description=root.find("detailed_description")[0].text,
+                criteria=eligibility.find("criteria")[0].text,
+                gender=eligibility.find("gender").text,
+                minimum_age=eligibility.find("minimum_age").text,
+                maximum_age=eligibility.find("maximum_age").text,
+                healthy_volunteers=eligibility.find("healthy_volunteers").text,
+            )
+        )
+
+    return clinical_trials
+
+
 if __name__ == "__main__":
     topic_file = "../data/external/topics2021.xml"
+    folder_name = "../data/external/ClinicalTrials"
 
     topics = parse_topics_from_xml(topic_file)
 
     print(topics)
+
+    cts = parse_clinical_trials_from_folder(folder_name=folder_name)
