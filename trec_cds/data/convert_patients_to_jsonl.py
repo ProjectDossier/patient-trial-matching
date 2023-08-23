cbed2242fe64d7c7289bad7f951f62c501966a23
Wojciech Kusa <wojciech.kusa@gmail.com>
2023-08-23 16:30:49 +0200

    refactor: refactor convert_patients_to_jsonl.py

--- trec_cds/data/convert_patients_to_jsonl.py
+++ trec_cds/data/convert_patients_to_jsonl.py
@@ -1,9 +1,11 @@
+import argparse
 import json
 from dataclasses import asdict
 from typing import List
 
 from CTnlp.patient import Patient
 from CTnlp.patient import load_patients_from_xml
+
 from trec_cds.features.drug_disease_extraction import EntityExtraction
 from trec_cds.features.entity_recognition import (
     EntityRecognition,
@@ -39,15 +41,23 @@ def convert_patients_to_jsonl(patients: List[Patient], outfile: str) -> None:
 
 
 if __name__ == "__main__":
-    DATA_PATH = "data/external/"
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input_data",
+        type=str,
+        default="data/external/",
+        help="Path to the folder containing the clinical trials.",
+    )
+    args = parser.parse_args()
+
     ee_model = EntityExtraction()
     er = EntityRecognition()
 
-    for in_file in ["topics2021", "topics2022"]:
+    for in_file in ["topics2021", "topics2022", "topics2023"]:
         patients = load_patients_from_xml(
-            f"{DATA_PATH}/{in_file}.xml", input_type="TREC"
+            f"{args.input_data}/{in_file}.xml", input_type="TREC"
         )
         er.predict(topics=patients)
-        OUTFILE = f"{DATA_PATH}/{in_file}.jsonl"
+        OUTFILE = f"{args.input_data}/{in_file}.jsonl"
 
         convert_patients_to_jsonl(patients=patients, outfile=OUTFILE)
